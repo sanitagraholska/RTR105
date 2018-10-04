0105b77615cbae7bc752ab99c9fa174eff16e9ec
user <slido3@inbox.lv>
2018-10-04 11:51:48 +0300

    20181004_11_51

---
+++ dialogs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/phython
+# -*- coding: utf-8 -*-
+a = input("Cien, liet, lūdzu ievadi skaitli: ")
+# 3. phyton'ā visi rezultāti ir ar str datu tipu
+# tapēc ievadītā lieluma datu tips vēlāk ir jāpārveido
+a = int(a)
+
+print("Liet, tu esi ievadījis skaitli : %d"%(a))
+aa = a * a
+print("Tavs skaitlis kvadrata ir : %d"%(aa))
+aa = a * a
+
